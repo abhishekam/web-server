2ef21568b969a285f0cc3405a5c553ea069a3ad7
Abhi <abhishekam.swamy@gmail.com>
2016-07-08 17:13:54 +0530

    Refactored middleware to separate file

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ var PORT = 3000;
 //     res.send("Hello, Express");
 // });
 
-var middleware = {
-    requestAuthentication: function(req, res, next){
-        console.log("pvt route hit!");
-        next();
-    },
-    logger: function(req, res, next){
-        console.log("Request: "+new Date().toString()+": "+req.method+": "+req.originalUrl);
-        next();
-    }
-};
-
+var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/about', middleware.requestAuthentication, function(req,res){
